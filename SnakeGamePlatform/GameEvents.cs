223f2b31c4023a79399b5a6d8648b8c68272b2d3
User <User@LAB1-09>
2024-03-06 14:20:34 +0200

    הוספנו אוכל

--- SnakeGamePlatform/GameEvents.cs
+++ SnakeGamePlatform/GameEvents.cs
@@ -13,7 +13,7 @@ namespace SnakeGamePlatform
     public class GameEvents:IGameEvents
     {
         //Define game variables here! for example...
-        GameObject[] snake;
+        GameObject[] snake, misgeret;
         TextLabel lblScore;
         GameObject right, left, down, up;
         int snakeSize;
@@ -23,20 +23,37 @@ namespace SnakeGamePlatform
         //Here you should initialize all variables defined above and create all visual objects on screen.
         //You could also start game background music here.
         //use board Object to add game objects to the game board, play background music, set interval, etc...
-        public int SnakeSize(GameObject[] arr)
+        public void Lose(GameObject[] snake, GameObject[] misgeret, Board Board, int snakeSize)
         {
-            int snakeSize = 0;
-            while (arr[snakeSize] != null)
+            if(snake[0].IntersectWith(misgeret[0]) || snake[0].IntersectWith(misgeret[1]) || snake[0].IntersectWith(misgeret[2]) || snake[0].IntersectWith(misgeret[3]))
             {
-                snakeSize++;
+               //Board.StopTimer();
             }
-            return snakeSize;
+            for (int i=1; i < snakeSize; i++)
+            {
+                if (snake[0].IntersectWith(snake[i]))
+                {
+                    //Board.StopTimer();
+                }
+            }
         }
+        public void AddFood (Board board)
+        {
+            Random rnd = new Random();
+            int y = rnd.Next(25,750);
+            int x = rnd.Next(245, 755);
+            
+            Position snakePosition = new Position(x, y);
+            GameObject food = new GameObject(snakePosition, 20, 20);
+            food.SetImage(Properties.Resources.food);
+            board.AddGameObject(food);
+        }
+        //לתקן את הלמעלה באוכל
         public void GameInit(Board board)
         {
 
             snake = new GameObject[24000];
-            snakeSize = SnakeSize(snake);
+            misgeret = new GameObject[4];
 
             //Setup board size and resolution!
             Board.resolutionFactor = 1;
@@ -73,27 +90,53 @@ namespace SnakeGamePlatform
             snake[3].SetImage(Properties.Resources.food);
             board.AddGameObject(snake[3]);
 
+            Position snakePosition4 = new Position(360, 300);
+            snake[4] = new GameObject(snakePosition3, 20, 20);
+            snake[4].SetImage(Properties.Resources.food);
+            board.AddGameObject(snake[4]);
 
-            //adding misgeret
-            Position boarddown = new Position(740,0) ;
-            down = new GameObject(boarddown, 800, 25);
-            down.SetBackgroundColor(Color.Black);
-            board.AddGameObject(down);
+            Position snakePosition5 = new Position(360, 300);
+            snake[5] = new GameObject(snakePosition3, 20, 20);
+            snake[5].SetImage(Properties.Resources.food);
+            board.AddGameObject(snake[5]);
+
+            Position snakePosition6 = new Position(360, 300);
+            snake[6] = new GameObject(snakePosition3, 20, 20);
+            snake[6].SetImage(Properties.Resources.food);
+            board.AddGameObject(snake[6]);
+
+            Position snakePosition7 = new Position(360, 300);
+            snake[7] = new GameObject(snakePosition3, 20, 20);
+            snake[7].SetImage(Properties.Resources.food);
+            board.AddGameObject(snake[7]);
+
+            Position snakePosition8 = new Position(360, 300);
+            snake[8] = new GameObject(snakePosition3, 20, 20);
+            snake[8].SetImage(Properties.Resources.food);
+            board.AddGameObject(snake[8]);
+
+            snakeSize =9;
+            //adding misgeret array
+
+            Position boarddown = new Position(775,0) ;
+            misgeret[0] = new GameObject(boarddown, 800, 25);
+            misgeret[0].SetBackgroundColor(Color.Black);
+            board.AddGameObject(misgeret[0]);
 
             Position boardup = new Position(175, 0);
-            up = new GameObject(boardup, 800, 25);
-            board.AddGameObject(up);
-            up.SetBackgroundColor(Color.Black);
+            misgeret[1] = new GameObject(boardup, 800, 25);
+            board.AddGameObject(misgeret[1]);
+            misgeret[1].SetBackgroundColor(Color.Black);
 
-            Position boardright = new Position(200, 760);
-            right = new GameObject(boardright, 25, 600);
-            board.AddGameObject(right);
-            right.SetBackgroundColor(Color.Black);
+            Position boardright = new Position(200, 775);
+            misgeret[2] = new GameObject(boardright, 25, 600);
+            board.AddGameObject(misgeret[2]);
+            misgeret[2].SetBackgroundColor(Color.Black);
 
             Position boardleft = new Position(200, 0);
-            left = new GameObject(boardleft, 25, 600);
-            board.AddGameObject(left);
-            left.SetBackgroundColor(Color.Black);
+            misgeret[3] = new GameObject(boardleft, 25, 600);
+            board.AddGameObject(misgeret[3]);
+            misgeret[3].SetBackgroundColor(Color.Black);
 
             //Play file in loop!
             board.PlayBackgroundMusic(@"\Images\gameSound.wav");
@@ -102,7 +145,7 @@ namespace SnakeGamePlatform
 
 
             //Start game timer!
-            board.StartTimer(50);
+            board.StartTimer(1);
         }
         
         
@@ -110,25 +153,29 @@ namespace SnakeGamePlatform
         //Use this function to move game objects and check collisions
         public void GameClock(Board board)
         {
+            int p = snakeSize-1;
             for (int i = 1; i < snakeSize; i++)
             {
-                int p = snakeSize;
-                snake[p - 1].SetPosition(snake[p].GetPosition());
+                
+                snake[p].SetPosition(snake[p-1].GetPosition());
                 p--;
             }
 
             Position snakePosition = snake[0].GetPosition();
             if (snake[0].direction == GameObject.Direction.RIGHT)
-                snakePosition.Y = snakePosition.Y + 5;
+                snakePosition.Y = snakePosition.Y + 20;
             if (snake[0].direction == GameObject.Direction.LEFT)
-                snakePosition.Y = snakePosition.Y - 5;
+                snakePosition.Y = snakePosition.Y - 20;
             if (snake[0].direction == GameObject.Direction.DOWN)
-                snakePosition.X = snakePosition.X + 5;
+                snakePosition.X = snakePosition.X + 20;
             if (snake[0].direction == GameObject.Direction.UP)
-                snakePosition.X = snakePosition.X - 5;
+                snakePosition.X = snakePosition.X - 20;
 
 
             snake[0].SetPosition(snakePosition);
+
+            Lose(snake, misgeret, board, snakeSize);
+            AddFood(board);
         }
 
         //This function is called by the game when the user press a key down on the keyboard.
